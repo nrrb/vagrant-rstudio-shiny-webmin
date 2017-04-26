d4396823805317979b765fc5c50889fdf7bc4e1b
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2017-04-25 23:49:27 -0500

    Vagrant config for RStudio Server, Shiny Server, and MySQL Server.

---
+++ usefulpackages.R
@@ -0,0 +1,14 @@
+options("repos"="http://cran.rstudio.com") # set the cran mirror
+
+packages = c("devtools","ggplot2","dplyr","tidyr","RcppEigen", "stringr","gridExtra",
+             "RCurl","RJSONIO","RJDBC","knitr","lme4","latticeExtra","RMySQL",
+             "XLConnect","Cairo","opencpu","rstudio")
+packages = setdiff(packages, installed.packages()[,"Package"])
+if (length(packages) != 0){
+  (install.packages(packages, dep=c("Depends", "Imports")))
+}
+
+# Packages from github are installed unconditionally
+ghpackages = c("rstudio/htmltools","trestletech/shinyTable","rstudio/rmarkdown","rstudio/shiny")
+devtools::install_github(ghpackages)
+update.packages(ask=FALSE)
